7976a8f18feb1b14904a59f36468e093d03e23ce
oibay <oibay@oibay.kz>
2024-05-13 16:06:34 +0500

    added: info domain

---
+++ src/Domains/Info.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Oibay\Ncanode\Domains;
+
+use Oibay\Ncanode\Domains\Constants\Action;
+use Oibay\Ncanode\Domains\Contracts\DomainInterface;
+
+class Info implements Contracts\DomainInterface
+{
+
+    private string $key;
+    private string $password;
+    private ?string $alies = null;
+
+    /**
+     * @param string $key
+     * @param string $password
+     * @param string|null $alies
+     */
+    public function __construct(string $key, string $password, ?string $alies)
+    {
+        $this->key = $key;
+        $this->password = $password;
+        $this->alies = $alies;
+    }
+
+
+    public function getAction(): string
+    {
+        return Action::PKCS12_INFO;
+    }
+
+    public function toArray(): array
+    {
+        return [
+           'revocationCheck' => [
+               'OCSP'
+           ],
+           'keys' => [
+              [
+                  'key' => $this->key,
+                  'password' => $this->password,
+                  'keyAlias' => $this->alies
+              ]
+           ]
+        ];
+    }
+}
